e0d3a0d8021a66f965c74606539e0665a69b1f51
TonySantiagoRodrigues <betovem19@hotmail.com>
2023-05-20 21:48:08 -0300

    Versão 1

---
+++ indexe.js
@@ -0,0 +1,33 @@
+var img0 = window.document.getElementById('ic0')
+var img1 = window.document.getElementById("ic1")
+var img2 = window.document.getElementById("ic2")
+var img3 = window.document.getElementById("ic3")
+var img4 = window.document.getElementById("ic4")
+var img5 = window.document.getElementById("ic5")
+var img6 = window.document.getElementById("ic6")
+var img9 = window.document.getElementById('ic9')
+
+function RolarParaDireta(){
+    img1.style ="display:none"
+    img2.style ="display:none"
+    img3.style ="display:none"
+    img4.style ="display:flex"
+    img5.style ="display:flex"
+    img6.style ="display:flex"
+    img0.style ="display:flex"
+    img9.style ="display:none"
+    
+}
+
+function RolarParaEsquerda(){
+    img1.style ="display:flex"
+    img2.style ="display:flex"
+    img3.style ="display:flex"
+    img4.style ="display:none"
+    img5.style ="display:none"
+    img6.style ="display:none"
+    img0.style ="display:none"
+    img9.style ="display:flex"
+    
+}
+
